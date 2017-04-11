b4174451c238caae9af96f3821b1389c5ed8e002
Maggie29 <zmagali@yahoo.com>
2017-04-11 15:20:16 -0500

    applove2

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+var products = ["Tayman", "Kerala", "The Brass Coq", "Sungenre", "Mitragyna Speciosa",
+ "Foam King", "Volition", "Ink", "Gilkon", "Owls & Apples", "Sherwood", "Platinum"]
+ //var listPicture = document.getElementsById("picture");
+
+ function print(event){
+   var listPicture = document.getElementById("picture");
+   for(var i = 0; i<products.length; i++){
+var divGrid = document.createElement("div");
+divGrid.setAttribute("class","grid");
+var figure = document.createElement("figure");
+var img = document.createElement("img");
+img.setAttribute("class", "image-box")
+img.src= "assets/img/fotos/img-"+ (i+1)+ ".jpg";
+img.alt = products[i];
+var figcaption = document.createElement("figcaption");
+figcaption.setAttribute("class", "center");
+var titulo = document.createElement("h4");
+titulo.setAttribute("class", "name")
+titulo.innerHTML = products[i];
+figcaption.appendChild(titulo);
+figure.appendChild(img);
+figure.appendChild(figcaption);
+divGrid.appendChild(figure);
+listPicture.appendChild(divGrid);
+}
+}
+window.addEventListener("load",print);
